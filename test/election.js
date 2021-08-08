e388670268ba247c019b65f72ffa8c06a360d92f
Assaf <assafo@gmail.com>
2021-08-08 16:42:56 +0530

    changed tests to use async/await instead of promise chains

--- test/election.js
+++ test/election.js
@@ -1,56 +1,49 @@
 var Election = artifacts.require("./Election.sol");
 
 contract("Election", async function(accounts) {
+  var instance;
   var electionInstance;
 
-  it("initializes with two candidates", function() {
-    return Election.deployed().then(function(instance) {
-      return instance.candidatesCount();
-    }).then(function(count) {
-      assert.equal(count, 2);
-    });
+  it("initializes with two candidates", async function() {
+    var instance = await Election.deployed();
+    var count = await instance.candidatesCount();
+    assert.equal(count, 2);
   });
 
-  it("it initializes the candidates with the correct values", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      return electionInstance.candidates(1);
-    }).then(function(candidate) {
-      assert.equal(candidate[0], 1, "contains the correct id");
-      assert.equal(candidate[1], "Candi 1", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
-      return electionInstance.candidates(2);
-    }).then(function(candidate) {
-      assert.equal(candidate[0], 2, "contains the correct id");
-      assert.equal(candidate[1], "Candi 2", "contains the correct name");
-      assert.equal(candidate[2], 0, "contains the correct votes count");
-    });
+  it("it initializes the candidates with the correct values", async function() {
+    var instance = await Election.deployed();
+    electionInstance = instance;
+
+    var candidate = await electionInstance.candidates(1);
+    assert.equal(candidate[0], 1, "contains the correct id");
+    assert.equal(candidate[1], "Candi 1", "contains the correct name");
+    assert.equal(candidate[2], 0, "contains the correct votes count");
+
+    candidate = await electionInstance.candidates(2);
+    assert.equal(candidate[0], 2, "contains the correct id");
+    assert.equal(candidate[1], "Candi 2", "contains the correct name");
+    assert.equal(candidate[2], 0, "contains the correct votes count");
   });
 
-  it("allows a voter to cast a vote", function() {
-    return Election.deployed().then(function(instance) {
-      electionInstance = instance;
-      candidateId = 1;
-      return electionInstance.vote(candidateId, { from: accounts[0] });
-    }).then(function(receipt) {
-      assert.equal(receipt.logs.length, 1, "an event was triggered");
-      assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
-      assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
-      return electionInstance.hasVoted(accounts[0]);
-    }).then(function(voted) {
-      assert(voted, "the voter was marked as voted");
-      return electionInstance.candidates(candidateId);
-    }).then(function(candidate) {
-      var voteCount = candidate[2];
-      assert.equal(voteCount, 1, "increments the candidate's vote count");
-    })
+  it("allows a voter to cast a vote", async function() {
+    var electionInstance = await Election.deployed();
+    var candidateId = 1;
+    var receipt = await electionInstance.vote(candidateId, { from: accounts[0] });
+    assert.equal(receipt.logs.length, 1, "an event was triggered");
+    assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
+    assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
+    var voted = await electionInstance.hasVoted(accounts[0]);
+    assert(voted, "the voter was marked as voted");
+    var candidate = await electionInstance.candidates(candidateId);
+    var voteCount = candidate[2];
+    assert.equal(voteCount, 1, "increments the candidate's vote count");
   });
 
   it("throws an exception for invalid candidates", function() {
     return Election.deployed().then(function(instance) {
       electionInstance = instance;
       return electionInstance.vote(99, { from: accounts[1] })
-    }).then(assert.fail).catch(function(error) {
+    }).then(assert.fail).catch(function(error) { // TODO learn how to catch error using async. Leaving chain for now.
       assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
       return electionInstance.candidates(1);
     }).then(function(candidate1) {
@@ -74,7 +67,7 @@ contract("Election", async function(accounts) {
       assert.equal(voteCount, 1, "accepts first vote");
       // Try to vote again
       return electionInstance.vote(candidateId, { from: accounts[1] });
-    }).then(assert.fail).catch(function(error) {
+    }).then(assert.fail).catch(function(error) { // TODO learn how to catch error using async. Leaving chain for now.
       assert(error.message.indexOf('revert') >= 0, "error message must contain revert");
       return electionInstance.candidates(1);
     }).then(function(candidate1) {
